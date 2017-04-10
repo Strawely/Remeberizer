e0456d0964ecf875bef2bd2a057001fc2b8a2324
Straw <solomyanniy63@gmail.com>
2017-04-10 09:59:05 +0400

    okay...

--- app/src/main/java/com/example/solom/rememberizer/MainMenuActivity.java
+++ app/src/main/java/com/example/solom/rememberizer/MainMenuActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-//    okay
+//    okay...
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
